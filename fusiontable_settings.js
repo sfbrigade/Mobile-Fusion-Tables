4ad309024c52121fde6190504eccb56d22aa22dd
danielk123 <kokkalisdaniel@gmail.com>
2015-09-28 15:38:27 +0300

    Update fusiontable_settings.js

--- fusiontable_settings.js
+++ fusiontable_settings.js
@@ -30,11 +30,11 @@ var MapsLib = MapsLib || {}; MapsLib.schemaVersion = 2;
     // See https://developers.google.com/fusiontables/docs/v1/migration_guide for more info
 
     // The encrypted Table ID of your Fusion Table (found under File => About)
-    MapsLib.fusionTableId = "13xmU6wANRJb0Niqcdz5Tr0xWh4AyV0yN10xMxPc";
+    MapsLib.fusionTableId = "1MlnS3u4ch2m7TACq--CnOedJLhHYrMt8WdhqCz1F";
 
     // *New Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
     // *Important* this key is for demonstration purposes. please register your own.
-    MapsLib.googleApiKey ="AIzaSyAMVBSXes-6P-gWaxRj20GK8NT6WDVpozM";
+    MapsLib.googleApiKey ="AIzaSyDEDViSqR9rsYe_H33PAamKbWlt8zK3K4M";
     
 
     // DONE!  YOU COULD DELETE EVERYTHING AFTER THIS POINT AND STILL HAVE A WORKING APP.
